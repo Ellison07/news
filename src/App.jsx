aa41f963522373438fde51207e93d50c79dc608c
Ellison07 <ellisonyurembam@gmail.com>
2023-07-26 23:08:59 +0530

    update kappu

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import NewsList from './components/NewsList';
-import './App.css'; 
+import './App.css';
 
 const App = () => {
   const [searchQuery, setSearchQuery] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize] = useState(8);
+  const [totalPages, setTotalPages] = useState(1);
 
   const handleSearchChange = (event) => {
     setSearchQuery(event.target.value);
+    setCurrentPage(1); // Reset to the first page when search query changes
   };
 
-  // Categories array
+  // Categories array (same as before)
   const categories = [
     'All',
     'Politics', // Adding 'All' as a category to show all news
@@ -25,19 +30,38 @@ const App = () => {
     'Travel',
     'Lifestyle',
     'War',
-    // Add other categories here
+    // ...
   ];
 
-  // Function to handle category selection
+  // Function to handle category selection (same as before)
   const handleCategorySelect = (category) => {
-    // Set the search query to the selected category
     setSearchQuery(category.toLowerCase());
+    setCurrentPage(1); // Reset to the first page when category is selected
+  };
+
+  useEffect(() => {
+    // Fetch the total number of articles for pagination
+    const getTotalArticles = async () => {
+      const response = await axios.get(
+        `https://newsapi.org/v2/everything?q=${searchQuery ? searchQuery : '*'}&apiKey=52b603dcce4a4085b26887595775e123`
+      );
+      const totalArticles = response.data.totalResults;
+      setTotalPages(Math.ceil(totalArticles / pageSize));
+    };
+    getTotalArticles();
+  }, [searchQuery, pageSize]);
+
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+  };
+
+  const handlePrevPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
   };
 
   return (
     <div className='App'>
       <div className='nav-bar'>
-        {/* News Categories */}
         <div className='nav-links'>
           <h1>News Categories</h1>
           <b></b>
@@ -46,10 +70,7 @@ const App = () => {
               Categories
               <div className='dropdown-content'>
                 {categories.map((category, index) => (
-                  <a
-                    key={index}
-                    onClick={() => handleCategorySelect(category)}
-                  >
+                  <a key={index} onClick={() => handleCategorySelect(category)}>
                     {category}
                   </a>
                 ))}
@@ -57,7 +78,6 @@ const App = () => {
             </button>
           </div>
         </div>
-        {/* Search News Bar */}
         <div className='search-container'>
           <h1>Search news</h1>
           <input
@@ -69,7 +89,15 @@ const App = () => {
           />
         </div>
       </div>
-      <NewsList searchQuery={searchQuery} />
+      <NewsList searchQuery={searchQuery} currentPage={currentPage} pageSize={pageSize} />
+      <div className='pagination'>
+        <button onClick={handlePrevPage} disabled={currentPage === 1}>
+          Previous
+        </button>
+        <button onClick={handleNextPage} disabled={currentPage === totalPages}>
+          Next
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -1,34 +1,36 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import { NewsItem } from '.'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import NewsItem from './NewsItem';
 
+const NewsList = ({ searchQuery, currentPage, pageSize }) => {
+  const [articles, setArticles] = useState([]);
 
-const NewsList = ({ searchQuery }) => {
-    const [articles, setArticles] = useState([])
+  useEffect(() => {
+    const getArticles = async () => {
+      const response = await axios.get(
+        `https://newsapi.org/v2/everything?q=${searchQuery ? searchQuery : '*'}&apiKey=52b603dcce4a4085b26887595775e123&page=${currentPage}&pageSize=${pageSize}`
+      );
+      setArticles(response.data.articles);
+    };
 
-    useEffect(() => {
-        const getArticles = async () => {
-            const response = await axios.get(`https://newsapi.org/v2/everything?q=${searchQuery?searchQuery:"*"}&apiKey=52b603dcce4a4085b26887595775e123`)
-            setArticles(response.data.articles)
-            console.log(response)
-        }
+    getArticles();
+  }, [searchQuery, currentPage, pageSize]);
 
-        getArticles()
-    }, [searchQuery])
-    return (
-        <div>
-            {articles.map(article => {
-                return(
-                    <NewsItem 
-                        title={article.title}
-                        description={article.description}
-                        url={article.url}
-                        urlToImage={article.urlToImage} 
-                    />
-                )
-            })}
-        </div>
-    )
-}
+  return (
+    <div className='news-list'>
+      {articles.map((article) => {
+        return (
+          <NewsItem
+            key={article.title}
+            title={article.title}
+            description={article.description}
+            url={article.url}
+            urlToImage={article.urlToImage}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
-export default NewsList
+export default NewsList;
